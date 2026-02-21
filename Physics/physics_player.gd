fdd31ddbf05bbbe48e562e310d8fc7b16246a34b
SandbergHM <53910983+SandbergHM@users.noreply.github.com>
2026-02-21 15:12:03 +0100

    Explosion test
    Right-click explosion test working

--- Physics/physics_player.gd
+++ Physics/physics_player.gd
@@ -6,17 +6,16 @@ extends RigidBody2D
 @export var impulse_power: float = 50.0
 ##Explosion range
 @export var range: float = 100.0
+##Explosion destruction range
+@export var destruction_range: float = 50.0
 
 func _process (delta):
 	#Player movement
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		
 		#get mouse position
 		var mouse_pos = get_global_mouse_position()
-		
 		#get mouse direction from player
 		var mouse_dir = global_position.direction_to(mouse_pos)
-		
 		#apply force
 		apply_impulse(mouse_dir * hit_force)
 	
@@ -24,13 +23,17 @@ func _process (delta):
 	if Input.is_action_just_pressed("explosion"):
 		for node: Node in get_parent().get_children():
 			if node is RigidBody2D:
-				#Get mouse position
-				var mouse_position = get_global_mouse_position()
-				#Mouse --> Node direction
-				var direction: Vector2 = node.global_position - mouse_position
-				#Mouse --> Node distance
+
+				#Player --> Node direction
+				var direction: Vector2 = node.global_position - position
+				#Player --> Node distance
 				var distance: float = direction.length()
+				var classname = node.get_scene_file_path()
 				
-				#Apply force if within range
-				if distance < range:
-					node.apply_impulse(direction.normalized() * impulse_power)
+				if node.get_scene_file_path() == "res://Physics/crate.tscn":
+					#Destroy crate if too close to explosion
+					if (distance < destruction_range):
+						node.queue_free()
+					#Apply force if within range
+					elif (distance < range):
+						node.apply_impulse(direction.normalized() * impulse_power)
