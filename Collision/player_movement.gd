81dd39c02c62744e3bf0f08195040654c4e0d332
Marcus Sandberg <marcus.sandberg@carel.com>
2026-02-24 14:20:41 +0100

    Added simple jump
    Collision game jump added

--- Collision/player_movement.gd
+++ Collision/player_movement.gd
@@ -2,17 +2,34 @@ extends RigidBody3D
 
 ##player right-left movement speed
 @export var move_speed : float = 3
+##player jump height
+@export var jump_force : float = 1
 
-# Called 60 times per second. 'delta' is the elapsed time since the previous frame.
+@onready var ray = $GroundDetector
+
+func is_on_floor() -> bool:
+	return ray.is_colliding()
+
+func _process(delta):
+	#Restart if player falls too low
+	if position.y < -20:
+		get_tree().reload_current_scene()
+		
+	print(is_on_floor())
+
+
 func _physics_process(delta: float) -> void:
 	if Input.is_physical_key_pressed(KEY_A):
 		apply_force(Vector3.LEFT * move_speed)
 	if Input.is_physical_key_pressed(KEY_D):
 		apply_force(Vector3.RIGHT * move_speed)
+	if Input.is_physical_key_pressed(KEY_SPACE) and is_on_floor():
+		apply_impulse(Vector3.UP * jump_force)
 		
-	if position.y < -20:
-		get_tree().reload_current_scene()
 
+
+
+#Restart if player collides with tree
 func _on_body_entered(body: Node):
 	if body.is_in_group("Tree"):
 		get_tree().reload_current_scene()
